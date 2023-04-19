d57df0558162648fead4e60aab8722df15f86f5e
Shree <109030986+Shree-77@users.noreply.github.com>
2023-04-19 20:41:51 +0530

    Project completed

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,49 @@ class TicTacToe extends StatefulWidget {
 }
 
 class _TicTacToeState extends State<TicTacToe> {
-  List<String> _gameState = List.filled(9, "");
-  String _currentPlayer = "X";
+  List<String> game = List.filled(9, "1");
+  String player1 = "X";
+  String player2 = "O";
+  String currentPlayer = "";
 
-  void _playMove(int index) {
+  void handleMove(int index) {
+
     setState(() {
-      if (_gameState[index] == "") {
-        _gameState[index] = _currentPlayer;
-        _currentPlayer = _currentPlayer == "X" ? "O" : "X";
+      game[index] = currentPlayer;
+      if(currentPlayer==player1){
+        currentPlayer=player2;
+      }else{
+        currentPlayer=player1;
       }
     });
   }
 
-  void _resetGame() {
-    setState(() {
-      _gameState = List.filled(9, "");
-      _currentPlayer = "X";
-    });
+  bool checkWin(String symbol) {
+
+    for (int i = 0; i < 9; i += 3) {
+      if (game[i] == symbol && game[i + 1] == symbol && game[i + 2] == symbol) {
+        return true;
+      }
+    }
+
+    for (int i = 0; i < 3; i++) {
+      if (game[i] == symbol && game[i + 3] == symbol && game[i + 6] == symbol) {
+        return true;
+      }
+    }
+
+    if (game[0] == symbol && game[4] == symbol && game[8] == symbol) {
+      return true;
+    }
+    if (game[2] == symbol && game[4] == symbol && game[6] == symbol) {
+      return true;
+    }
+
+    return false;
+  }
+
+  void ResetGame() {
+    game = List.filled(9, " ");
   }
 
   @override
@@ -41,16 +67,89 @@ class _TicTacToeState extends State<TicTacToe> {
         children: [
           GridView.builder(
             gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
             shrinkWrap: true,
-            itemCount: _gameState.length,
+            itemCount: game.length,
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: ElevatedButton(
-                  onPressed: () => _playMove(index),
+                  onPressed: () => {
+                    setState(
+                      () {
+                        if (game[index] == " ") {
+                          handleMove(index);
+                          if (checkWin(player1)) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Game Over'),
+                                  content: Text('Player 1 Wins!'),
+                                  actions: [
+                                    ElevatedButton(
+                                      child: Text('OK'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                        setState(() {
+                                          ResetGame();
+                                        });
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+
+                          } else if (checkWin(player2)) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Game Over'),
+                                  content: Text('Player 2 Wins!'),
+                                  actions: [
+                                    ElevatedButton(
+                                      child: Text('OK'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                        setState(() {
+                                          ResetGame();
+                                        });
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else if (!game.contains(" ")) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Game Over'),
+                                  content: Text('Match Tie '),
+                                  actions: [
+                                    ElevatedButton(
+                                      child: Text('OK'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                        setState(() {
+                                          ResetGame();
+                                        });
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        }
+                      },
+                    )
+                  },
                   child: Text(
-                    _gameState[index],
+                    game[index],
                     style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
                   ),
                 ),
@@ -59,7 +158,11 @@ class _TicTacToeState extends State<TicTacToe> {
           ),
           SizedBox(height: 20),
           ElevatedButton(
-            onPressed: _resetGame,
+            onPressed: () => {
+              setState(() {
+                ResetGame();
+              })
+            },
             child: Text("Reset Game"),
           ),
         ],
